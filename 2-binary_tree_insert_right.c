322ec95e55b1748799f8427f2aaaf3596285b44e
gulrah <gulnarrahimli@mail.ru>
2024-01-09 03:58:17 -0800

    task2_betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,26 +8,26 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node, *temp;
+binary_tree_t *new_node, *temp;
 
-    if (parent == NULL)
-        return NULL;
+if (parent == NULL)
+return NULL;
 
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return NULL;
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+return NULL;
 
-    if (parent->right != NULL)
-    {
-        temp = parent->right;
-        parent->right = new_node;
-        new_node->right = temp;
-        temp->parent = new_node;
-    }
-    else
-    {
-        parent->right = new_node;
-    }
+if (parent->right != NULL)
+{
+temp = parent->right;
+parent->right = new_node;
+new_node->right = temp;
+temp->parent = new_node;
+}
+else
+{
+parent->right = new_node;
+}
 
-    return new_node;
+return new_node;
 }
