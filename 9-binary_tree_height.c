2895f335aee34245f3edf94393bc1f3a24411f23
gulrah <gulnarrahimli@mail.ru>
2024-01-10 12:34:22 -0800

    9again

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,27 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_size - Measures the size of a binary tree
  *
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: Pointer to the root node of the tree to measure the size
  *
- * Return: Height of the binary tree, or 0 if tree is NULL
+ * Return: Size of the binary tree, or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-    /* Base Case: If the tree is empty (NULL), height is 0 */
+    size_t left_size, right_size;
+
+    /* Base Case: If the tree is empty (NULL), size is 0 */
     if (tree == NULL)
         return 0;
 
-    /* Recursive Case: Compute the height of the left subtree */
-    size_t left_height = binary_tree_height(tree->left);
+    /* Recursive Case: Compute the size of the left subtree */
+    left_size = binary_tree_size(tree->left);
 
-    /* Recursive Case: Compute the height of the right subtree */
-    size_t right_height = binary_tree_height(tree->right);
+    /* Recursive Case: Compute the size of the right subtree */
+    right_size = binary_tree_size(tree->right);
 
-    /* Return the maximum height between left and right subtrees, plus 1 for the current node */
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+    /* Return the total size, which is the sum of the sizes of left and right subtrees, plus 1 for the current node */
+    return (left_size + right_size + 1);
 }
