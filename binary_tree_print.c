2b9a793ad6d4e82fe3548885fdf7a8d7b08a5525
gulrah <gulnarrahimli@mail.ru>
2024-01-10 05:56:10 -0800

    task0_betty

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -9,19 +9,19 @@
  */
 void print_tree(const binary_tree_t *tree, int space, int height)
 {
-    if (tree == NULL)
-        return;
+if (tree == NULL)
+return;
 
-    space += height;
+space += height;
 
-    print_tree(tree->right, space, height);
+print_tree(tree->right, space, height);
 
-    printf("\n");
-    for (int i = height; i < space; i++)
-        printf(" ");
-    printf("%d\n", tree->n);
+printf("\n");
+for (int i = height; i < space; i++)
+printf(" ");
+printf("%d\n", tree->n);
 
-    print_tree(tree->left, space, height);
+print_tree(tree->left, space, height);
 }
 
 /**
@@ -30,9 +30,9 @@ void print_tree(const binary_tree_t *tree, int space, int height)
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+if (tree == NULL)
+return;
 
-    int height = 5;
-    print_tree(tree, 0, height);
+int height = 5;
+print_tree(tree, 0, height);
 }
