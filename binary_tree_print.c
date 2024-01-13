aff02ba1dd1c137be665ce65da9915ccb51fa8cc
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-01-12 22:58:33 -0800

    fixing

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 #include <stdio.h>
-
 /**
  * print_tree - Recursive function to print a binary tree
  * @tree: Pointer to the root node of the tree to print
@@ -9,30 +8,26 @@
  */
 void print_tree(const binary_tree_t *tree, int space, int height)
 {
-if (tree == NULL)
-return;
-
-space += height;
-
-print_tree(tree->right, space, height);
-
-printf("\n");
-for (int i = height; i < space; i++)
-printf(" ");
-printf("%d\n", tree->n);
-
-print_tree(tree->left, space, height);
+	if (tree == NULL)
+		return;
+	space += height;
+	print_tree(tree->right, space, height);
+	printf("\n");
+	for (int i = height; i < space; i++)
+		printf(" ");
+	printf("%d\n", tree->n);
+	print_tree(tree->left, space, height);
 }
-
 /**
  * binary_tree_print - Prints a binary tree
  * @tree: Pointer to the root node of the tree to print
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return;
+	if (tree == NULL)
+		return;
+	int height = 5;
 
-int height = 5;
-print_tree(tree, 0, height);
+	print_tree(tree, 0, height);
+
 }
